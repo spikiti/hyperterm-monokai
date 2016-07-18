943b002f43a9925cbce6a94b094766674b9412bb
Badr Chaouai <badr.chaouai@gmail.com>
2016-07-18 21:27:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# hyperterm-monokai
+
+> Monokai theme for [HyperTerm](https://hyperterm.org)
+
+![](screenshot.png)
+
+## Install
+
+Add `hyperterm-monokai` to the plugins list in your `~/.hyperterm.js` config file.
+
+## License
+
+MIT © [Badr Chaouai](http://arkhamdev.com)
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+const backgroundColor = '#282828';
+const foregroundColor = '#F8F8F0';
+
+const colors = [
+  backgroundColor,
+  '#F92672', // pink
+  '#A6E22E', // green
+  '#FD971F', // orange
+  '#AE81FF', // purple
+  '#66D9EF', // blue
+  '#38CCD1', // cyan
+  '#ffffff', // white
+  '#49483E', // brown gray
+  '#F92672', // pink
+  '#A6E22E', // green
+  '#FD971F', // orange
+  '#AE81FF', // purple
+  '#66D9EF', // blue
+  '#38CCD1', // cyan
+  '#ffffff', // white
+  foregroundColor
+];
+
+exports.decorateConfig = (config) => (
+  Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    cursorColor: foregroundColor,
+    colors,
+    css: `
+      ${config.css || ''}
+      .tab_tab {
+        color: ${foregroundColor} !important;
+        background-color: ${backgroundColor} !important;
+      }
+      .tab_active:before {
+        border-bottom-color: #E6DB74 !important;
+      }
+    `
+  })
+);
